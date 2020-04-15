9a3faef2761b154ad3515e5ae50fa6c888c8ca53
Ramzi <ramzi.el12@gmail.com>
2020-04-15 01:10:52 -0500

    chore: added netlify back

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f5b127efcb35d2dabbeb";
+/******/ 	var hotCurrentHash = "2b2a965bc6a100d33b55";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import { Component } from 'react';
+import Head from "next/head"
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
 
-class Index extends Component {
-  render(){
+export default class Home extends Component {
+  render() {
+    let { title, cats } = attributes;
     return (
       <>
-        <h1>Ramzi El-Ramahi</h1>
+        <Head>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
       </>
     )
   }
 }
-
-export default Index;
